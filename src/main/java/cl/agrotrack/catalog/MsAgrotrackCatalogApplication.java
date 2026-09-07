c1ef161fb5c032ddf4896bb84f3c57e142693c73
Diego Otárola <dieg.otarola@duocuc.cl>
2026-09-07 10:39:40 -0400

    chore: esqueleto inicial de ms-agrotrack-catalog

---
+++ src/main/java/cl/agrotrack/catalog/MsAgrotrackCatalogApplication.java
@@ -0,0 +1,13 @@
+package cl.agrotrack.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAgrotrackCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAgrotrackCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/agrotrack/catalog/MsAgrotrackCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.agrotrack.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAgrotrackCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
