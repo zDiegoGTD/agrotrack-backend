40361791b412bada9b9338998e46e32613d616d7
Diego Otárola <dieg.otarola@duocuc.cl>
2026-09-07 10:39:41 -0400

    chore: esqueleto inicial de ms-agrotrack-audit

---
+++ src/main/java/cl/agrotrack/audit/MsAgrotrackAuditApplication.java
@@ -0,0 +1,13 @@
+package cl.agrotrack.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAgrotrackAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAgrotrackAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/agrotrack/audit/MsAgrotrackAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.agrotrack.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAgrotrackAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
