edf15fc8d39908e903aeadd41ba31f4bcc87820f
Diego Otárola <dieg.otarola@duocuc.cl>
2026-09-07 10:39:39 -0400

    chore: esqueleto inicial de ms-agrotrack-deliveries

---
+++ src/main/java/cl/agrotrack/deliveries/MsAgrotrackDeliveriesApplication.java
@@ -0,0 +1,13 @@
+package cl.agrotrack.deliveries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAgrotrackDeliveriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAgrotrackDeliveriesApplication.class, args);
+	}
+
+}
